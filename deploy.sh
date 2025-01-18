c74a52e0fd7b08e74f119416dd239632cd5bf002
ryanluc7reis <ryan.dev18@outlook.com>
2025-01-18 13:39:04 -0300

    Adjusments on script deploy

--- deploy.sh
+++ deploy.sh
@@ -34,65 +34,16 @@ After=network.target
 [Service]
 User=main
 WorkingDirectory=/home/main/closebynearme_testRyan
-ExecStart=/usr/local/bin/yarn admin-prod
-ExecStart=/usr/local/bin/yarn web-prod
-ExecStart=/usr/local/bin/yarn backend-prod
+ExecStart=/bin/bash -c "/usr/local/bin/yarn admin-prod && /usr/local/bin/yarn web-prod && /usr/local/bin/yarn backend-prod"
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
 EOF'
 
-# Build and run the backend environment
-#cd ../backend
-
-
-# Configure systemd for backend
-#sudo bash -c 'cat > /etc/systemd/system/backend.service <<EOF
-#[Unit]
-#Description=Backend Service
-#After=network.target
-
-#[Service]
-#User=main
-#WorkingDirectory=/home/main/closebynearme_testRyan/backend
-#ExecStart=/usr/local/bin/yarn start
-#Restart=always
-
-#[Install]
-#WantedBy=multi-user.target
-#EOF'
-
-# Build and run the web environment
-#cd ../web
-#yarn install --frozen-lockfile
-#yarn build
-
-# Configure systemd for web
-#sudo bash -c 'cat > /etc/systemd/system/web.service <<EOF
-#[Unit]
-#Description=Web Service
-#After=network.target
-
-#[Service]
-#User=main
-#WorkingDirectory=/home/main/closebynearme_testRyan/web
-#ExecStart=/usr/local/bin/yarn start
-#Restart=always
-
-#[Install]
-#WantedBy=multi-user.target
-#EOF'
-
 # Reload and start the services
 sudo systemctl daemon-reload
 sudo systemctl start app
-#sudo systemctl start admin
-#sudo systemctl start backend
-#sudo systemctl start web
-#sudo systemctl enable admin
-#sudo systemctl enable backend
-#sudo systemctl enable web
 
 ENDSSH
 
