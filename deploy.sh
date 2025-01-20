ee1c5f9a94109039019305f1e9c4d4b3f5626d55
ryanluc7reis <ryan.dev18@outlook.com>
2025-01-19 21:59:46 -0300

    Adjusments on script deploy and next config from web

--- deploy.sh
+++ deploy.sh
@@ -10,62 +10,63 @@ sshpass -e ssh main@64.23.152.172 << 'ENDSSH'
 sudo dpkg --configure -a
 
 # Update and install dependencies
-sudo apt-get update
-sudo apt-get install -y nodejs npm docker.io git
+#sudo apt-get update
+#sudo apt-get install -y nodejs npm docker.io git
 
 
 # Clone the repository
-git clone https://github.com/Ryanluc7reis/closebynearme_testRyan.git
+#git clone https://github.com/Ryanluc7reis/closebynearme_testRyan.git
 cd closebynearme_testRyan
 
 # Run the container in the deploy environment (up the mongo and redis from my database)
-cd deploy
-docker-compose up -d --build
+#cd deploy
+#docker-compose up -d --build
 
 # Build and run the app environment
-cd ../admin
-yarn install
-yarn build
+#cd ../admin
+#yarn install
+#yarn build
 
 # Configure systemd for  admin
-sudo bash -c 'cat > /etc/systemd/system/admin.service <<EOF
-[Unit]
-Description=Admin Service
-After=network.target
+#sudo bash -c 'cat > /etc/systemd/system/admin.service <<EOF
+#[Unit]
+#Description=Admin Service
+#After=network.target
 
-[Service]
-User=main
-WorkingDirectory=/home/main/closebynearme_testRyan
-ExecStart=/usr/local/bin/yarn start
-Restart=always
+#[Service]
+#User=main
+#WorkingDirectory=/home/main/closebynearme_testRyan
+#ExecStart=/usr/local/bin/yarn start
+#Restart=always
 
-[Install]
-WantedBy=multi-user.target
-EOF'
+#[Install]
+#WantedBy=multi-user.target
+#EOF'
 
 # Build and run the backend environment
-cd ../backend
+#cd ../backend
 
 # Configure systemd for backend
-sudo bash -c 'cat > /etc/systemd/system/backend.service <<EOF
-[Unit]
-Description=Backend Service
-After=network.target
+#sudo bash -c 'cat > /etc/systemd/system/backend.service <<EOF
+#[Unit]
+#Description=Backend Service
+#After=network.target
 
-[Service]
-User=main
-WorkingDirectory=/home/main/closebynearme_testRyan/backend
-ExecStart=/usr/local/bin/yarn start
-Restart=always
+#[Service]
+#User=main
+#WorkingDirectory=/home/main/closebynearme_testRyan/backend
+#ExecStart=/usr/local/bin/yarn start
+#Restart=always
 
-[Install]
-WantedBy=multi-user.target
-EOF'
+#[Install]
+#WantedBy=multi-user.target
+#EOF'
 
 # Build and run the web environment
-cd ../web
-yarn install
-yarn build
+cd web
+yarn install --production
+export NODE_OPTIONS="--max-old-space-size=512"
+yarn build --verbose
 
 # Configure systemd for web
 sudo bash -c 'cat > /etc/systemd/system/web.service <<EOF
@@ -73,7 +74,7 @@ sudo bash -c 'cat > /etc/systemd/system/web.service <<EOF
 Description=Web Service
 After=network.target
 
-[Service]
+#[Service]
 User=main
 WorkingDirectory=/home/main/closebynearme_testRyan/web
 ExecStart=/usr/local/bin/yarn start
@@ -87,10 +88,10 @@ EOF'
 sudo systemctl daemon-reload
 #sudo systemctl start admin
 #sudo systemctl start backend
-#sudo systemctl start web
+sudo systemctl start web
 #sudo systemctl enable admin
 #sudo systemctl enable backend
-#sudo systemctl enable web
+sudo systemctl enable web
 
 ENDSSH
 
--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,17 +1,44 @@
-import withTM from 'next-transpile-modules';
+// import withTM from 'next-transpile-modules';
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = withTM([
+//   'react-native',
+//   'react-native-web'
+// ])({
+//   reactStrictMode: true,
+//   output: 'standalone',
+//   // experimental: {
+//   //   turbo: {
+//   //     optimizePackageImports: ['package-name']
+//   //   }
+//   // },
+//   images: {
+//     remotePatterns: [
+//       {
+//         hostname: 'closebynearme.s3.amazonaws.com',
+//         protocol: 'https',
+//         port: ''
+//       }
+//     ]
+//   },
+//   // webpack(config) {
+//   //   config.resolve.alias = {
+//   //     ...(config.resolve.alias || {}),
+//   //     'react-native$': 'react-native-web'
+//   //   };
+
+//   //   config.resolve.extensions = ['.web.js', '.js', '.ts', '.tsx', '.json', '.jsx'];
+
+//   //   return config;
+//   // },
+//   staticPageGenerationTimeout: 300
+// });
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = withTM([
-  'react-native',
-  'react-native-web'
-])({
+const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  experimental: {
-    turbo: {
-      optimizePackageImports: ['package-name']
-    }
-  },
   images: {
     remotePatterns: [
       {
@@ -21,17 +48,7 @@ const nextConfig = withTM([
       }
     ]
   },
-  webpack(config) {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      'react-native$': 'react-native-web'
-    };
-
-    config.resolve.extensions = ['.web.js', '.js', '.ts', '.tsx', '.json', '.jsx'];
-
-    return config;
-  },
-  staticPageGenerationTimeout: 300
-});
+  staticPageGenerationTimeout: 600
+};
 
 export default nextConfig;
