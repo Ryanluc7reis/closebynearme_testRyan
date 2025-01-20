78ce3a5557a22b3ac48e80e86ff5bb385926dc42
ryanluc7reis <ryan.dev18@outlook.com>
2025-01-19 23:20:39 -0300

    Adjusments on  script deploy

--- deploy.sh
+++ deploy.sh
@@ -23,45 +23,48 @@ cd closebynearme_testRyan
 #docker-compose up -d --build
 
 # Build and run the app environment
-cd admin
-yarn install
-export NODE_OPTIONS="--max-old-space-size=768"
-NEXT_DEBUG=1 yarn build
+#cd admin
+#yarn install
+#export NODE_OPTIONS="--max-old-space-size=768"
+#NEXT_DEBUG=1 yarn build
 
 # Configure systemd for  admin
-sudo bash -c 'cat > /etc/systemd/system/admin.service <<EOF
-[Unit]
-Description=Admin Service
-After=network.target
-
-[Service]
-User=main
-WorkingDirectory=/home/main/closebynearme_testRyan
-ExecStart=/usr/local/bin/yarn start
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF'
-
-# Build and run the backend environment
-#cd ../backend
-
-# Configure systemd for backend
-#sudo bash -c 'cat > /etc/systemd/system/backend.service <<EOF
+#sudo bash -c 'cat > /etc/systemd/system/admin.service <<EOF
 #[Unit]
-#Description=Backend Service
+#Description=Admin Service
 #After=network.target
 
 #[Service]
 #User=main
-#WorkingDirectory=/home/main/closebynearme_testRyan/backend
+#WorkingDirectory=/home/main/closebynearme_testRyan/admin
 #ExecStart=/usr/local/bin/yarn start
 #Restart=always
 
 #[Install]
 #WantedBy=multi-user.target
 #EOF'
+
+# Build and run the backend environment
+cd backend
+yarn install
+export NODE_OPTIONS="--max-old-space-size=768"
+NEXT_DEBUG=1 yarn build
+
+# Configure systemd for backend
+sudo bash -c 'cat > /etc/systemd/system/backend.service <<EOF
+[Unit]
+Description=Backend Service
+After=network.target
+
+[Service]
+User=main
+WorkingDirectory=/home/main/closebynearme_testRyan/backend
+ExecStart=/usr/local/bin/yarn start
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF'
 
 # Build and run the web environment
 #cd web
@@ -87,11 +90,11 @@ EOF'
 
 # Reload and start the services
 sudo systemctl daemon-reload
-sudo systemctl start admin
-#sudo systemctl start backend
+#sudo systemctl start admin
+sudo systemctl start backend
 #sudo systemctl start web
-sudo systemctl enable admin
-#sudo systemctl enable backend
+#sudo systemctl enable admin
+sudo systemctl enable backend
 #sudo systemctl enable web
 
 ENDSSH
