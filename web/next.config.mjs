59254e0b7a18b7da50f472b63976850a94a4fb53
ryanluc7reis <ryan.dev18@outlook.com>
2025-01-19 22:31:40 -0300

    Adjusments on next config from web

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,44 +1,17 @@
-// import withTM from 'next-transpile-modules';
+import withTM from 'next-transpile-modules';
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = withTM([
-//   'react-native',
-//   'react-native-web'
-// ])({
-//   reactStrictMode: true,
-//   output: 'standalone',
-//   // experimental: {
-//   //   turbo: {
-//   //     optimizePackageImports: ['package-name']
-//   //   }
-//   // },
-//   images: {
-//     remotePatterns: [
-//       {
-//         hostname: 'closebynearme.s3.amazonaws.com',
-//         protocol: 'https',
-//         port: ''
-//       }
-//     ]
-//   },
-//   // webpack(config) {
-//   //   config.resolve.alias = {
-//   //     ...(config.resolve.alias || {}),
-//   //     'react-native$': 'react-native-web'
-//   //   };
-
-//   //   config.resolve.extensions = ['.web.js', '.js', '.ts', '.tsx', '.json', '.jsx'];
-
-//   //   return config;
-//   // },
-//   staticPageGenerationTimeout: 300
-// });
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withTM([
+  'react-native',
+  'react-native-web'
+])({
   reactStrictMode: true,
   output: 'standalone',
+  // experimental: {
+  //   turbo: {
+  //     optimizePackageImports: ['package-name']
+  //   }
+  // },
   images: {
     remotePatterns: [
       {
@@ -48,7 +21,17 @@ const nextConfig = {
       }
     ]
   },
-  staticPageGenerationTimeout: 600
-};
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'react-native$': 'react-native-web'
+    };
+
+    config.resolve.extensions = ['.web.js', '.js', '.ts', '.tsx', '.json', '.jsx'];
+
+    return config;
+  },
+  staticPageGenerationTimeout: 300
+});
 
 export default nextConfig;
