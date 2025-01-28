200f506220680fecec9156964275f901ffc92212
ryanluc7reis <ryan.dev18@outlook.com>
2025-01-27 22:52:35 -0300

    Commenting imports not used

--- backend/src/seller/seller.resolver.ts
+++ backend/src/seller/seller.resolver.ts
@@ -3,8 +3,8 @@ import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { SellerService } from './seller.service';
 import { CreateSellerInput } from './dto/create-seller.input';
 //import { SellerPaginateResponse } from './entities/seller-paginate';
-import { UseGuards } from '@nestjs/common';
-import { AuthGuard } from '../auth/auth.guard';
+//import { UseGuards } from '@nestjs/common';
+//import { AuthGuard } from '../auth/auth.guard';
 import { Seller } from './entities/seller.entity';
 
 @Resolver(() => Seller)
