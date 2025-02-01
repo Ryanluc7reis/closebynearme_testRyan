e19884963033c5cf9fc6ea56b09da9e6f40b9c9f
ryanluc7reis <ryan.dev18@outlook.com>
2025-01-31 22:31:31 -0300

    Removing fucntion unnecessary

--- backend/src/seller/seller.resolver.ts
+++ backend/src/seller/seller.resolver.ts
@@ -3,7 +3,7 @@ import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { SellerService } from './seller.service';
 import { CreateSellerInput } from './dto/create-seller.input';
 import { UpdateSellerInput } from './dto/update-seller.input';
-import { LoginInputSeller } from 'src/auth/dto/login-input.seller';
+
 //import { SellerPaginateResponse } from './entities/seller-paginate';
 //import { UseGuards } from '@nestjs/common';
 //import { AuthGuard } from '../auth/auth.guard';
@@ -17,11 +17,6 @@ export class SellerResolver {
   async createSeller(@Args('input') input: CreateSellerInput) {
     return this.sellerService.create(input);
   }
-  @Mutation(() => String)
-  async loginSeller(@Args('input') input: LoginInputSeller) {
-    return this.sellerService.loginSeller(input);
-  }
-
   @Mutation(() => String)
   async updateSeller(@Args('input') input: UpdateSellerInput) {
     return this.sellerService.updateSeller(input);
