b21ccf95023d5b9fb2c4a9593eda1b834505a97a
ryanluc7reis <ryan.dev18@outlook.com>
2025-01-31 21:55:27 -0300

    Created login on seller resolver

--- backend/src/seller/seller.resolver.ts
+++ backend/src/seller/seller.resolver.ts
@@ -3,6 +3,7 @@ import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { SellerService } from './seller.service';
 import { CreateSellerInput } from './dto/create-seller.input';
 import { UpdateSellerInput } from './dto/update-seller.input';
+import { LoginInputSeller } from 'src/auth/dto/login-input.seller';
 //import { SellerPaginateResponse } from './entities/seller-paginate';
 //import { UseGuards } from '@nestjs/common';
 //import { AuthGuard } from '../auth/auth.guard';
@@ -15,6 +16,10 @@ export class SellerResolver {
   @Mutation(() => String)
   async createSeller(@Args('input') input: CreateSellerInput) {
     return this.sellerService.create(input);
+  }
+  @Mutation(() => String)
+  async loginSeller(@Args('input') input: LoginInputSeller) {
+    return this.sellerService.findOne(input);
   }
 
   @Mutation(() => String)
